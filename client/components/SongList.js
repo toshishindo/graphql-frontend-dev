e00056fdb090f11d223f71b46c280edafb09bf95
Tony Shindo <honolulu3310@gmail.com>
2018-06-04 20:54:24 -0500

    Add navigation icon

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
 
 class SongList extends Component {
   renderSongs() {
@@ -14,23 +15,31 @@ class SongList extends Component {
   }
 
   render() {
-    if (this.props.data.loading) { return <div>Loading...</div>; }
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
 
     return (
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
-    );  
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link
+          to="songs/new"
+          className="btn-floating btn-large red right"
+        >
+          <i className="material-icons">add</i>  
+        </Link>
+      </div>
+    );
   }
 }
 
 const query = gql`
-{
-  songs {
-    id
-    title
+  {
+    songs {
+      id
+      title
+    }
   }
-}
 `;
 
 export default graphql(query)(SongList);
