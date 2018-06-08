e0ac69fa8d95f5f3399a7b6d58ef6d79bda7fbfa
Tony Shindo <honolulu3310@gmail.com>
2018-06-07 22:19:29 -0500

    Create add lyric page

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,7 +6,8 @@ import query from "../queries/fetchSongs";
 
 class SongList extends Component {
   onSongDelete(id) {
-    this.props.mutate({ variables: { id } })
+    this.props
+      .mutate({ variables: { id } })
       .then(() => this.props.data.refetch());
   }
 
@@ -14,7 +15,9 @@ class SongList extends Component {
     return this.props.data.songs.map(({ id, title }) => {
       return (
         <li key={id} className="collection-item">
-          {title}
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
           <i className="material-icons" onClick={() => this.onSongDelete(id)}>
             delete
           </i>
